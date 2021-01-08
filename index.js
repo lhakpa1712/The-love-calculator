3ba76409fcc4007aa6a6363453a75e7fb5cd0567
lhakpa1712 <69619866+lhakpa1712@users.noreply.github.com>
2021-01-08 16:15:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function calculate(){
+    let num = Math.random(0,10);
+        num = Math.floor(num * 100)
+    if(num > 70){
+            alert(num+"%"+ "😍😍" + "You guys love each other like kanye loves kanye");
+        } 
+        else{
+            alert(num+"%" +"🤪🤪" + "You guys are not in love");
+        }
+        
+        
+    
+}
+    
